78ae8533b93b66776afc14ca2b27efbcac267a35
Manish <manishkulal54@gmail.com>
2022-09-19 20:03:05 +0530

    light dark mode updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,7 +6,7 @@ import Navbar from './Navbar'
 import beginningImg from '../Images/project/beginningProject.png'
 import textUtilAppImg from '../Images/project/textUtilsApp.png'
 import foodRecipeImg from '../Images/project/foodRecipe.png'
-import iCloudNotebook from '../Images/project/iCloudNotebook.png'
+import iCloudNotebook from '../Images/project/ICloudNoteBook.png'
 
 export default function Projects(props) {
     return (
@@ -20,7 +20,7 @@ export default function Projects(props) {
                 <h2 className='mt-7'>Projects</h2>
             <div className="container mt-3 mb-3 d-flex flex-wrap justify-content-around align-items-center">
 
-                <div className="card mb-5" style={{width: "18rem",height:'max-content'}}>
+                <div className={`card mb-5 ${props.mode!=='dark'?'lightmode':'darkmode backDarkMode'}`} style={{width: "18rem",height:'max-content'}}>
                     <img src={beginningImg} className="card-img-top" alt="First project Img"/>
                         <div className="card-body">
                             <h5 className="card-title">First combined project</h5>
@@ -28,7 +28,7 @@ export default function Projects(props) {
                             <a href="https://bbqlnow0karreseypovyxq.on.drv.tw/mywebsite/main.html" target={"blank"} className="btn btn-primary">Open project</a>
                         </div>
                 </div>
-                <div className="card mb-5" style={{width: "18rem",height:'max-content'}}>
+                <div className={`card mb-5 ${props.mode!=='dark'?'lightmode':'darkmode backDarkMode'}`} style={{width: "18rem",height:'max-content'}}>
                     <img src={textUtilAppImg} className="card-img-top" alt="textutil project Img"/>
                         <div className="card-body">
                             <h5 className="card-title">TextUtils App</h5>
@@ -36,7 +36,7 @@ export default function Projects(props) {
                             <a href="https://manishkulal-textutil.netlify.app" target={"blank"} className="btn btn-primary">Open project</a>
                         </div>
                 </div>
-                <div className="card mb-5" style={{width: "18rem",height:'max-content'}}>
+                <div className={`card mb-5 ${props.mode!=='dark'?'lightmode':'darkmode backDarkMode'}`} style={{width: "18rem",height:'max-content'}}>
                     <img src={foodRecipeImg} className="card-img-top" alt="Food recipe project Img"/>
                         <div className="card-body">
                             <h5 className="card-title">Food Recipe</h5>
@@ -44,7 +44,7 @@ export default function Projects(props) {
                             <a href="https://manishkulal-foodrecipe.netlify.app/" target={"blank"} className="btn btn-primary">Open project</a>
                         </div>
                 </div>
-                <div className="card mb-5" style={{width: "18rem",height:'max-content'}}>
+                <div className={`card mb-5 ${props.mode!=='dark'?'lightmode':'darkmode backDarkMode'}`} style={{width: "18rem",height:'max-content'}}>
                     <img src={iCloudNotebook} className="card-img-top" alt="Icloudnotebook project Img"/>
                         <div className="card-body">
                             <h5 className="card-title">ICloudNoteBook</h5>
